c088d200574614c08f68e5f2cfe519f708d40a1c
Zanet <zanetschneidermann@gmail.com>
2024-10-12 16:19:21 +0200

    meta6

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -80,7 +80,9 @@ export default function File(props) {
       {/* Facebook share */}
       <div className="absolute bottom-2 right-2 gap-4 mx-4 flex flex-row">
         <div>
-          <FacebookShareButton url="https://pictureit.vercel.app/">
+          <FacebookShareButton
+            url={`https://pictureit-fvuigmur9-zanets-projects.vercel.app/image-page/${props.file._id.toString()}`}
+          >
             <FacebookIcon size={40} round={true} />
           </FacebookShareButton>
         </div>
